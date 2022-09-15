5a114973fe68fcf4595df4e8fcd547eb42be57c4
Rakib <rakib@think-engineer.com>
2022-09-15 16:22:14 +0100

    email events for bookings + reqs

--- routes/sg.js
+++ routes/sg.js
@@ -60,4 +60,124 @@ router.post("/lp-contact-us", async (req, res) => {
   }
 });
 
+router.post("/booking-request-received", async (req, res) => {
+  let { tutorEmail, studentName, lesson, lessonTime, price } = req.body;
+
+  const msg = {
+    from: {
+      email: "tutors@rktutors.co.uk", // Change to your recipient
+      name: "RKTutors Team",
+    },
+    to: tutorEmail, // Change to your verified sender
+
+    templateId: "d-9f91fb96719543cbb45c3341dcda357f",
+    dynamic_template_data: { studentName, lesson, lessonTime, price },
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+    res.json({ success: true, message: "sent email" });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
+router.post("/booking-request-accepted", async (req, res) => {
+  let { studentEmail, tutorName, subject, date } = req.body;
+
+  const msg = {
+    from: {
+      email: "tutors@rktutors.co.uk", // Change to your recipient
+      name: "RKTutors Team",
+    },
+    to: studentEmail, // Change to your verified sender
+
+    templateId: "d-b6e0de8b6f484f53aeacf25104b7792f",
+    dynamic_template_data: { tutorName, subject, date },
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+    res.json({ success: true, message: "sent email" });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
+router.post("/booking-request-declined", async (req, res) => {
+  let { studentEmail, tutorName, subject, date } = req.body;
+
+  const msg = {
+    from: {
+      email: "tutors@rktutors.co.uk", // Change to your recipient
+      name: "RKTutors Team",
+    },
+    to: studentEmail, // Change to your verified sender
+
+    templateId: "d-852dc85e9a7740499e97368edc4f2174",
+    dynamic_template_data: { tutorName, subject, date },
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+    res.json({ success: true, message: "sent email" });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
+router.post("/booking-request-cancelled", async (req, res) => {
+  let { tutorEmail, studentName, lesson, time } = req.body;
+
+  const msg = {
+    from: {
+      email: "tutors@rktutors.co.uk", // Change to your recipient
+      name: "RKTutors Team",
+    },
+    to: tutorEmail, // Change to your verified sender
+
+    templateId: "d-690e370db4f44d64a8311e514af4a467",
+    dynamic_template_data: { studentName, lesson, time },
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+    res.json({ success: true, message: "sent email" });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
+router.post("/booking-cancelled", async (req, res) => {
+  let { toEmail, userName, subject, date } = req.body;
+
+  const msg = {
+    from: {
+      email: "tutors@rktutors.co.uk", // Change to your recipient
+      name: "RKTutors Team",
+    },
+    to: toEmail, // Change to your verified sender
+
+    templateId: "d-8aff5a3711cd40fab437e54060fc0f89",
+    dynamic_template_data: { userName, subject, date },
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+    res.json({ success: true, message: "sent email" });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
 module.exports = router;
